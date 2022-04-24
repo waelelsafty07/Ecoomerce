7c6d8b67346da1bdacfbd2c36892940267a86f38
waelismael <UGS.140589@ci.suez.edu.eg>
2022-04-24 17:57:36 +0200

    test webhook

--- controller/orderController.js
+++ controller/orderController.js
@@ -158,6 +158,7 @@ exports.checkoutSession = asyncHandler(async (req, res, next) => {
 });
 
 exports.webhookCheckout = asyncHandler(async (req, res, next) => {
+  console.log(process.env.STRIPE_END_POINT);
   const sig = req.headers["stripe-signature"];
 
   let event;
