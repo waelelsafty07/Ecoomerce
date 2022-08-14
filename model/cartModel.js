22207782ad96d7e7e1f8c7585f3db5dd0af904da
waelismael <UGS.140589@ci.suez.edu.eg>
2022-08-14 18:58:52 +0200

    update cart

--- model/cartModel.js
+++ model/cartModel.js
@@ -43,4 +43,12 @@ const cartSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
+cartSchema.pre(/^find/, function (next) {
+  this.populate({
+    path: "cartItems.product",
+    select: "-_id",
+  });
+  next();
+});
+
 module.exports = mongoose.model("Cart", cartSchema);
